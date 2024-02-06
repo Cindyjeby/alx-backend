57c949dcca89c1efc186cd997d9c3160ca25f8ea
cindyjeby <cindyjeby@gmail.com>
2024-02-06 10:58:30 +0000

    debug 1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
-from flask import Flask
+"""
+A Basic Flask app.
+"""
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
 
 @app.route('/')
-def hello():
-    return 'Hello, Flask'
+def get_index() -> str:
+    """
+    The home/index page.
+    """
+    return render_template('0-index.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
