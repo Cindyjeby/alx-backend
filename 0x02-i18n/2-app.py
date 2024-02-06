5b6d259d32efbb992a904678af80966c0bad1373
cindyjeby <cindyjeby@gmail.com>
2024-02-06 19:07:27 +0000

    get locale3

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""A basic fask app"""
-
+"""A Basic Flask app.
+"""
 from flask_babel import Babel
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 class Config:
-    """reps the flask babel config"""
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -16,15 +18,20 @@ app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale() -> str:
-    """retrives the locale of a webpage"""
-    return request.accept_language.best_match(app.config["LANGUAGES"])
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
 
 @app.route('/')
 def get_index() -> str:
-    """index page"""
+    """The home/index page.
+    """
     return render_template('2-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
